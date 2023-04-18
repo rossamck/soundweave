66c2466c6c654622bcee7fe17a8286543f83471d
ross <ross.mckeown1@gmail.com>
2023-04-18 19:29:07 +0100

    updated cmakelists to produce 2 executables for testing

--- src/Networking/holepunch.cpp
+++ src/Networking/holepunch.cpp
@@ -1,25 +1,12 @@
 #include "holepunch.h"
 
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
-const int BUFFER_SIZE = 1024;
-
-struct ClientInfo
-    {
-    sockaddr_in addr;
-    };
 
 
-class UDPSocket {
-public:
-  UDPSocket(const std::string& hostname, int port) {
+
+
+
+
+UDPSocket::UDPSocket(const std::string& hostname, int port) {
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     // std::cout << "Socket = " << sockfd << std::endl;
     if (sockfd < 0) {
@@ -38,12 +25,12 @@ public:
     }
   }
 
-  ~UDPSocket() {
+UDPSocket::~UDPSocket() {
     std::cout << "The UDP instance has gone out of scope, but socket not closed" << std::endl;
     // close(sockfd);
   }
 
-  bool send(const std::string& message, const std::string& hostname, int port) {
+bool UDPSocket::send(const std::string& message, const std::string& hostname, int port) {
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
@@ -58,7 +45,7 @@ public:
     return true;
   }
 
-  bool receive(std::string& message, std::string& sender_ip, int& sender_port, const std::string& listen_ip) {
+bool UDPSocket::receive(std::string& message, std::string& sender_ip, int& sender_port, const std::string& listen_ip) {
     // Set up the listen address
     sockaddr_in listen_addr;
     std::memset(&listen_addr, 0, sizeof(listen_addr));
@@ -87,7 +74,7 @@ public:
     return true;
 }
 
- int getBoundPort() {
+int UDPSocket::getBoundPort() {
     // Set up the socket address
     sockaddr_in socket_addr;
     socklen_t addrlen = sizeof(socket_addr);
@@ -102,20 +89,16 @@ public:
     return ntohs(socket_addr.sin_port);
 }
 
-  int getSock() 
+int UDPSocket::getSock()
   {
     return sockfd;
   }
 
 
-private:
-  int sockfd;
-  int port_;
-};
   
 
-bool checkSender(std::string received_ip, std::string target_ip)
-{
+bool checkSender(std::string received_ip, std::string target_ip) {
+
     if (received_ip != target_ip)
     {
         std::cout << "No match" << std::endl;
--- src/Networking/holepunch.h
+++ src/Networking/holepunch.h
@@ -1,19 +1,56 @@
-#ifndef HEADER_H
-#define HEADER_H
+// holepunch.h
+#ifndef HOLEPUNCH_H
+#define HOLEPUNCH_H
 
+#include <string>
+#include <netinet/in.h>
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+const int BUFFER_SIZE = 1024;
+
+struct ClientInfo {
+    sockaddr_in addr;
+};
 
 struct ipInformation {
-  char ip[16];
-  int port;
-  int own_port;
-  int sock;
+    char ip[16];
+    int port;
+    int own_port;
+    int sock;
 
-  void print() const {
-    std::cout << "ip: " << ip << ", port: " << port << ", own port: " << own_port << std::endl;
-  }
+    void print() {
+        std::cout << "Client IP: " << ip << " Client Port: " << port << " Own Port: " << own_port << std::endl;
+    }
 };
+
+class UDPSocket {
+public:
+    UDPSocket(const std::string& hostname, int port);
+
+    ~UDPSocket();
+
+    bool send(const std::string& message, const std::string& hostname, int port);
+
+    bool receive(std::string& message, std::string& sender_ip, int& sender_port, const std::string& listen_ip);
+
+    int getBoundPort();
+
+    int getSock();
+
+private:
+    int sockfd;
+    int port_;
+};
+
+bool checkSender(std::string received_ip, std::string target_ip);
 
 ipInformation connectToClient();
 
-#endif
+#endif // HOLEPUNCH_H
