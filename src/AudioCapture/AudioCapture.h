ff2e3f8d05827fe06fe82083bfb352fe143afc01
ross <ross.mckeown1@gmail.com>
2023-04-20 15:04:28 +0100

    reverted files

--- src/AudioCapture/AudioCapture.h
+++ src/AudioCapture/AudioCapture.h
@@ -1,31 +1,110 @@
 #ifndef AUDIO_CAPTURE_H
 #define AUDIO_CAPTURE_H
 
-#include "RtAudio.h"
-#include <string>
+#include <iostream>
+#include <fstream>
 #include <vector>
-#include <functional>
+#include <cstring>
+#include <stdexcept>
+#include <thread>
+#include <cmath>
+#include <signal.h>
+#include <climits>
+#include <condition_variable>
+#include <mutex>
 
+#include <alsa/asoundlib.h>
+#include <fftw3.h>
+
+#include "PingPongBuffer.h"
+
+/**
+ * @class AudioCapture
+ * @brief A wrapper around the asound library for capturing audio.
+ */
 class AudioCapture {
 public:
-    using AudioDataCallback = std::function<void(const std::vector<short>&)>;
+    typedef void (*DataAvailableCallback)(const std::vector<short>&);
 
-    AudioCapture(const std::string& device_name = "", bool use_default_device = true);
+    /**
+     * @brief Constructor for this class
+     *
+     * Prompts user for audio device if not provided. 
+     * Gets handle for audio device and sets up necessary callbacks.
+     * @param device_name The name of the audio device to capture from.
+     */
+    AudioCapture(std::string device_name, bool dummy_audio);
+
+    /**
+     * @brief Registers callback for audio data
+     *
+     * Calls to this function is equivalent to subscribing to the data.
+     * Multiple subscribers can be set up by making multiple calls.
+     * @param cb A function pointer to the callback to be registered.
+     */
+    void register_callback(DataAvailableCallback cb);
+
+    /**
+     * @brief Prompts user to select audio device
+     *
+     * Lists all discoverable audio devices on system and prompts user to input a number selection.
+     * @return The name of the selected audio device.
+     */
+    std::string prompt_device_selection();
+
+    // Thread, mutex, and condition variable for the capture thread
+    std::thread captureThread;
+    std::mutex captureMutex;
+    std::condition_variable captureCv;
+    bool captureReady;
+    
+    /**
+     * @brief Destructor for this class
+     */
     ~AudioCapture();
 
-    void register_callback(AudioDataCallback callback);
-    void start();
-    void stop();
-
 private:
-    static int RtAudioCallback(void* outputBuffer, void* inputBuffer, unsigned int nBufferFrames,
-                               double streamTime, RtAudioStreamStatus status, void* userData);
-    unsigned int getDeviceId(const std::string& device_name);
 
-    RtAudio dac_;
-    std::string device_name_;
-    bool use_default_device_;
-    AudioDataCallback callback_;
+    /**
+     * @brief Callback function used to handle audio originating from hardware 
+     *
+     * This function reads the available audio data into a PingPongBuffer.
+     * Optionally, it renders a visualisation of the audio data.
+     * @param pcm_callback A pointer to the ALSA asynchronous callback handler.
+     */
+    static void MyCallback(snd_async_handler_t* pcm_callback);
+
+    /**
+     * @brief Calls all callback functions with audio data when PingPongBuffer is full
+     *
+     * This function reads iterates through all callbacks registered with 'register_callback'.
+     * Passes audio data from the PingPongBuffer, which is typically 4096 bytes.
+     * @param full_buffer A vector containing the audio data.
+     * @param size The number of elements in the audio data vector.
+     */
+    static void call_callbacks(const std::vector<short>& full_buffer, int size);
+
+    /**
+     * @brief Function to run the capturing loop in a separate thread
+     */
+    void CaptureThreadFunction();
+    void DummyAudioThreadFunction();
+
+    std::ofstream audioFile;
+    snd_pcm_t* handle;
+    snd_async_handler_t* pcm_callback;
+    std::vector<float> waveform;
+    static std::vector<DataAvailableCallback> callbacks;
+    static bool quit;
+
+    std::vector<short> fftInputData;
+
+    std::vector<int> tempbuffer;
+    DataAvailableCallback callback;
+
+    PingPongBuffer buffer_;
+
+    bool dummy_audio;
 };
 
-#endif // AUDIO_CAPTURE_H
+#endif  // AUDIO_CAPTURE_H
--- src/holepunchrtp2.cpp
+++ src/holepunchrtp2.cpp
@@ -1,5 +1,3 @@
-
-
 #include <iostream>
 #include <alsa/asoundlib.h>
 #include <sys/socket.h>
@@ -9,50 +7,48 @@
 #include <cstring>
 #include <stdexcept>
 #include <chrono>
-#include <memory>
 
 #include "Networking/holepunch.h"
 #include "Networking/RTP.h"
 #include "AudioCapture/AudioCapture.h"
 
-// Use a unique_ptr for the RTP object
-std::unique_ptr<RTP> rtp;
+// Global RTP object
+RTP *rtp = nullptr;
 int socket_fd = -1;
 
+
 void audio_data_callback(const std::vector<short>& audio_data) {
+    // Print the number of samples in the audio data
     std::cout << "Captured audio data with " << audio_data.size() << " samples" << std::endl;
 
-    if (rtp && socket_fd != -1) {
+    // You can process the audio data further here, e.g., save it to a file, analyze it, etc.
+       // Send audio data over RTP
+    if (rtp != nullptr && socket_fd != -1) {
         rtp->SendPacket(audio_data.data(), audio_data.size() * sizeof(short), socket_fd);
     }
 }
 
-int main() {
 
+int main()
+{
   std::cout << "Initialising..." << std::endl;
   ipInformation otherClient;
   otherClient = connectToClient();
   std::cout << "Main function: ip = " << otherClient.ip << " port = " << otherClient.port << " own port = " << otherClient.own_port << std::endl;
   std::cout << "Socket is: " << otherClient.sock << std::endl;
 
-
-
-
   const char *destination_ip = otherClient.ip;
   int destination_port = otherClient.port;
 
-  AudioCapture audioCapture("", true); //set true for dummy audio
+  AudioCapture audioCapture("", true); // Pass true for dummy_audio
   audioCapture.register_callback(audio_data_callback);
-  audioCapture.start();
 
-  // Instantiate the RTP object with a unique_ptr
-  rtp = std::make_unique<RTP>(otherClient.ip, otherClient.port, 11);
+  // Instantiate the RTP object
+  rtp = new RTP(otherClient.ip, otherClient.port, 11); // Use the appropriate payload type
   socket_fd = otherClient.sock;
 
   while (true) {
 
   }
-      audioCapture.stop(); // Stop the audio capture before exiting
-
   return 0;
 }
